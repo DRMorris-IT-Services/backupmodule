18fd44002b018b656cc3774d7becb5874387e97c
Duncan Morris <duncanmorris07@outlook.com>
2020-10-06 12:50:58 +0200

    setting up folder structure

---
+++ src/controllers/BackupControllerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BackupController;
+use Illuminate\Http\Request;
+
+class BackupControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BackupController  $backupController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BackupController $backupController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BackupController  $backupController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BackupController $backupController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BackupController  $backupController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BackupController $backupController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BackupController  $backupController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BackupController $backupController)
+    {
+        //
+    }
+}
