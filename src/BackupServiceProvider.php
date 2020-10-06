202aa16c3cd31a0e066171339b22cd9e6e1d779b
Duncan Morris <duncanmorris07@outlook.com>
2020-10-06 12:48:28 +0200

    first setup and testing

---
+++ src/BackupServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace duncanrmorris\backupmodule;
+
+use Illuminate\Support\ServiceProvider;
+
+class BackupServiceProvider extends ServiceProvider
+
+{
+    
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        //$this->loadViewsFrom(__DIR__.'/views','releasenotes');
+        //$this->mergeConfigFrom(__DIR__.'/config/releasenotes.php','releasenotes');
+        // $this->publishes([
+        //     __DIR__.'/Http/Controllers/ReleaseNotesController.php' => base_path('app/Http/Controllers/ReleaseNotesController.php'),
+        //     __DIR__.'/views/notes.blade.php' => base_path('resources/views/releasenotes/notes.blade.php'),
+
+        // ]);
+    }
+
+    public function register()
+    {
+        
+    }
+}
+
---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/backup', function(){
+    return 'Backup module';
+});
